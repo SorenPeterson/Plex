7f2f48b9bb45c0bcaa4ed2c534376b679522c49f
Soren Peterson <soren468@gmail.com>
2015-12-23 15:20:42 -0800

    getting somewhere on the syntax errors. JSlint really doesn't like my code... But thanks to jslint it will be better!

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
-(function(){
-  'use strict';
-})
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const exec = require('child_process').exec;
-const net = require('net');
-const http = require('http');
+/*jslint node: true */
+(function () {
+    'use strict';
 
-let mainWindow;
+    var electron = require('electron'),
+        app = electron.app,
+        BrowserWindow = electron.BrowserWindow,
+        exec = require('child_process').exec,
+        net = require('net'),
+        http = require('http'),
+        mainWindow;
 
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('ready', function() {
-  var child = exec('ssh -nNTL 32400:localhost:32400 tv@308jackson.ddns.net');
-
-  var openPage = function() {
-    mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      "node-integration": "iframe",
-      "web-preferences": {
-        "web-security": false
-      }
+    app.on('window-all-closed', function () {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
     });
 
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    app.on('ready', function () {
+        var child = exec('ssh -nNTL 32400:localhost:32400 tv@308jackson.ddns.net'),
+            openPage = function () {
+                mainWindow = new BrowserWindow({
+                    width: 800,
+                    height: 600,
+                    "node-integration": "iframe",
+                    "web-preferences": {
+                        "web-security": false
+                    }
+                });
 
-    mainWindow.on('closed', function() {
-      mainWindow = null;
-      child.kill();
-    });
-  };
+                mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  var connect = function() {
-    http.request('http://localhost:32400/web', function() {
-      openPage();
-    }).on('error', function(err) {
-      console.log(err);
-      setTimeout(function(), {
-        console.log('doge');
+                mainWindow.on('closed', function() {
+                    mainWindow = null;
+                    child.kill();
+                });
+            },
+            connect = function () {
+                http.request('http://localhost:32400/web', function() {
+                    openPage();
+                }).on('error', function(err) {
+                    console.log(err);
+                    setTimeout(function(), {
+                        console.log('doge');
+                        connect();
+                    });
+                });
+        }
+
         connect();
-      });
     });
-  }
 
-  connect();
-});
+}()); // This is the encapsulator
